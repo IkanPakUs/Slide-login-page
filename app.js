4b0ed88f6524630e75db89b2c5d52b01689dcf2a
MangArya11 <komanggede86@gmail.com>
2021-02-09 14:24:19 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,23 @@
+const sign_in_btn = document.querySelector("#sign-in-btn");
+const sign_up_btn = document.querySelector("#sign-up-btn");
+const container = document.querySelector(".container");
+
+sign_in_btn.addEventListener("click",() => {
+    container.classList.add("switch");
+    setTimeout(function(){
+        container.classList.remove("sign-up-mode")
+    }, 1500);
+    setTimeout(function(){
+        container.classList.remove("switch")
+    }, 2000);
+});
+
+sign_up_btn.addEventListener("click",() => {
+    container.classList.add("switch");
+    setTimeout(function(){
+        container.classList.add("sign-up-mode")
+    }, 1500);
+    setTimeout(function(){
+        container.classList.remove("switch")
+    }, 2000);
+});
